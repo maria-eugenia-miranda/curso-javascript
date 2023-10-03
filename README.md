b1c6ef5533e223b94d8d06674777ea6ab24bc5bd
Damian <damian@MacBook-Air-de-Damian.local>
2023-10-02 23:51:13 -0300

    Segunda preentrega js

--- README.md
+++ README.md
@@ -2,4 +2,18 @@
 Curso coderhouse javascript
 
 Alumna: Maria Eugenia Miranda Farias
-estudiante de CoderHouse
+Estudiante de CoderHouse
+
+Los productos a ingresar en el carrito, se encuentra precargados en el código y son los siguientes:
+
+Jean
+Remera
+Musculosa
+Buzo
+Joggins
+Campera
+Short
+
+#Códigos de descuento 
+VERANO2024 (Solo aplica descuento a los productos de verano)
+INVIERNO2023 (Solo aplica descuento a los productos de invierno)
--- scripts/index.js
+++ scripts/index.js
@@ -1,50 +1,134 @@
+
+
+class Producto {
+    constructor(articulo, precio, temporada) {
+        this.articulo = articulo;
+        this.precio = precio;
+        this.temporada = temporada;
+    }
+
+    sumarIva() {
+        this.precio = this.precio * 1.21;
+    }
+
+    aplicarDescuento(descuento) {
+        this.precio = this.precio - (this.precio * descuento);
+    }
+}
+
+
 // DECLARACIONES
 
 // DECLARAMOS VARIABLES
 let fin = false;
-let productos = "";
-let subTotal = 0;
+let productos = [];
+let carrito = [];
+let listProductos = '';
+
 
 // DECLARAMOS FUNCIÓN
-const aplicarDescuento = (subTotal, codigoDescuento) => {
+const aplicarDescuento = (codigoDescuento, productos) => {
+    let descuento;
     switch (codigoDescuento) {
-        case "PRIMAVERA2023":
-            return subTotal * 0.80;
-
+        case "VERANO2024":
+            descuento = 0.10;
+            for (const producto of productos) {
+                if (producto.temporada === 'Verano') {
+                    producto.aplicarDescuento(descuento);
+                }
+            }
             break;
-        case "DIADELAMADRE23":
-            return subTotal * 0.70;
-
+        case "INVIERNO2023":
+            descuento = 0.25; 
+            for (const producto of productos) {
+                if (producto.temporada === 'Invierno') {
+                    producto.aplicarDescuento(descuento);
+                }
+            }
             break;
         default:
-            return subTotal
             break;
     }
+}
+
+
+const calcularTotal = (carrito) => {
+    let total = 0;
+    for (const prod of carrito) {
+        listProductos += prod.articulo + ",";
+        total += prod.precio;
+    }
+    return total;
 }
 // FIN DECLARACIONES
 
+productos.push (new Producto ("jean",8000,"invierno"));
+productos.push (new Producto ("remera",4000,"verano"));
+productos.push (new Producto ("musculosa",3200, "verano"));
+productos.push (new Producto ("buzo",6600,"invierno"));
+productos.push (new Producto ("joggins",5000, "invierno"));
+productos.push (new Producto ("campera",10400,"invierno"));
+productos.push (new Producto ("short",5500,"verano"));
+
+
+
+
+// FUNCIONES DE ORDEN SUPERIOR
+
+productos.forEach((producto) => {
+    console.log(producto)
+}
+)
+
+for ( producto of productos){
+    producto.sumarIva ();
+    }
+
+ for(producto of productos) {
+    console.log(producto.precio);
+  };
+
+  const invierno = productos.filter ((producto) => producto.temporada.includes ("invierno"));
+  console.log (invierno)
+
+  const verano = productos.filter ((producto) => producto.temporada.includes ("verano"));
+  console.log (verano)
+
+  const productosActualizados = productos.map((producto) => {
+      return { 
+        articulo: producto.articulo,
+        precio: Math.floor(producto.precio * 1.15),
+        temporada: producto.temporada,
+    }
+});
+console.log(productosActualizados)
+
+// FIN DE FUNCIONES DE ORDEN SUPERIOR
+
+
 // EJECUCIÓN
 while (!fin) {
-    let producto = prompt("Ingrese nombre del producto, en caso de finalizar colocar FIN");
-
-    if (producto.toUpperCase() == "FIN") {
+    let productoIngresado = prompt("Ingrese nombre del producto, en caso de finalizar colocar FIN");
+    if (productoIngresado.toUpperCase() == "FIN") {
         fin = true;
     } else {
-        let costo = prompt("Ingrese el valor del producto");
-        productos += producto + " ";
-        subTotal += parseInt(costo);
-        console.log("Producto: " + producto + ". Costo: " + costo);
+        const productoSeleccionado = productos.find(producto => producto.articulo.toUpperCase() == productoIngresado.toUpperCase());
+        if (productoSeleccionado) {
+            carrito.push(productoSeleccionado);
+            alert('El valor del producto es: ' + productoSeleccionado.precio);
+        } else {
+            alert('El producto no existe.');
+        }
     }
+}
+const subTotal = calcularTotal(carrito);
+
+const codigoDescuento = prompt("Los productos seleccionados fueron " + listProductos + " Y el importe Total es " + subTotal + ". Si tiene un código de descuento , ingreselo. Sino presione aceptar para continuar")
+if (codigoDescuento != "") {
+    aplicarDescuento(codigoDescuento.toUpperCase(), carrito);
+    const total = calcularTotal(carrito);
+    alert("El total con el descuento aplicado es de $" + total + ".Gracias por tu compra");
+} else {
+    alert("El total es $" + subTotal + ". Gracias por tu compra");
 }
 
-const codigoDescuento = prompt("Los productos seleccionados fueron " + productos + " Y el importe Total es " + subTotal + ". Si tiene un código de descuento , ingreselo. Sino presione aceptar para continuar")
-if (codigoDescuento != "") {
-    const total = aplicarDescuento(subTotal, codigoDescuento.toUpperCase());
-    if (total == subTotal) {
-        alert("El código de descuento ingresado no es válido. Importe total: $" + total + ". Gracias por tu compra")
-    } else {
-        alert("El total con el descuento aplicado es de $" + total + ".Gracias por tu compra")
-    }
-} else {
-    alert("El total es $" + subTotal + ". Gracias por tu compra")
-}
